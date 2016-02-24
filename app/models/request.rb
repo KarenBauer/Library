303e2c1f645dc4670f368c8e428aa10d4f0c2891
Karen <karenbauerpej@gmail.com>
2016-02-24 18:15:34 -0300

    Filter status in request validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,7 +6,7 @@ class Request < ActiveRecord::Base
   validate :is_in_use?
 
   def is_in_use?
-    unless self.class.where(from_date: from_date..to_date).where.not(id: self.id).empty? && self.class.where(to_date: from_date..to_date).where.not(id: self.id).empty?
+    unless self.class.where(from_date: from_date..to_date, status: 1).where.not(id: self.id).empty? && self.class.where(to_date: from_date..to_date, status: 1).where.not(id: self.id).empty?
       errors.add(:atention, ": The book in this dates is not available")
     end
   end
